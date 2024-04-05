6aaf86b7c362764a90e9a2a8addf130f7a19547c
AlaaShokry76 <alaa.shokry760@gmail.com>
2024-04-05 21:26:26 +0200

    updated footer design

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,35 +7,33 @@ import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <>
-      <div className="w-full  ">
-        <div
-          dir="rtl"
-          className="flex justify-around items-center border-2 border-[#00000059] p-6 ">
-          <div dir="rtl" className="flex items-center gap-x-8 ">
-            <NavLink to="/instagram" target="_blank" rel="noopener noreferrer">
-              <img src={instagram} alt="instagram" />
-            </NavLink>
-            <NavLink to="/facebook" target="_blank" rel="noopener noreferrer">
-              <img src={facebook} alt="facebook" />
-            </NavLink>
-            <NavLink to="/email" target="_blank" rel="noopener noreferrer">
-              <img src={email} alt="email" />
-            </NavLink>
-          </div>
-          <div dir="rtl">
-            <img src={Logo} alt="Logo" />
-          </div>
-          <div dir="rtl">
-            <NavLink
-              to="/AboutUs"
-              className="text-[#196B69] text-[18px] leading-[20px] tracking-[0.25px]">
-              <p> معلومات عنا</p>
-            </NavLink>
-          </div>
+    <footer className="fixed bottom-0 left-0 w-full">
+      <div
+        dir="rtl"
+        className="flex justify-around items-center border-2 border-[#00000059] p-6">
+        <div dir="rtl" className="flex items-center gap-x-8">
+          <NavLink to="/instagram" target="_blank" rel="noopener noreferrer">
+            <img src={instagram} alt="instagram" />
+          </NavLink>
+          <NavLink to="/facebook" target="_blank" rel="noopener noreferrer">
+            <img src={facebook} alt="facebook" />
+          </NavLink>
+          <NavLink to="/email" target="_blank" rel="noopener noreferrer">
+            <img src={email} alt="email" />
+          </NavLink>
+        </div>
+        <div dir="rtl">
+          <img src={Logo} alt="Logo" />
+        </div>
+        <div dir="rtl">
+          <NavLink
+            to="/AboutUs"
+            className="text-[#196B69] text-[18px] leading-[20px] tracking-[0.25px]">
+            <p> معلومات عنا</p>
+          </NavLink>
         </div>
       </div>
-    </>
+    </footer>
   );
 };
 
--- client/src/pages/MedicalHistory.jsx
+++ client/src/pages/MedicalHistory.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MedicalHistory = () => {
   return (
     <div>
-      
+      ddddddd
     </div>
   )
 }
