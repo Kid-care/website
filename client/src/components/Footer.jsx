48a1c16f3beed52662c56b1db4c53c515b9187fb
RadwaNagy44 <radwanagy561@gmail.com>
2024-05-03 15:13:33 +0300

    Home is done

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,39 @@
+// Footer.jsx
 import React from "react";
-
+import instagram from "../assets/instagram.svg";
+import facebook from "../assets/facebook.svg";
+import email from "../assets/email.svg";
+import Logo from "../assets/LOGO.svg";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
     <>
-      
+      <footer className=" w-full relative  " dir="rtl">
+        <div className="bg-[#ffff] flex justify-around items-center z-999 border-2 border-[#00000059] p-3  ">
+          <div className="flex items-center gap-x-8 mr-20">
+            <NavLink to="/instagram" target="_blank">
+              <img src={instagram} alt="instagram" />
+            </NavLink>
+            <NavLink to="/facebook" target="_blank">
+              <img src={facebook} alt="facebook" />
+            </NavLink>
+            <NavLink to="/email" target="_blank">
+              <img src={email} alt="email" />
+            </NavLink>
+          </div>
+          <div>
+            <img src={Logo} alt="Logo" />
+          </div>
+          <div>
+            <NavLink
+              to="/AboutUs"
+              className="text-[#196B69] text-[18px] leading-[20px] tracking-[0.25px] font-[Roboto] font-semibold	">
+              <p> معلومات عنا</p>
+            </NavLink>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
