bcdcb5a3a19c06332b3538a88e0ac77b11e04315
AlaaShokry76 <alaa.shokry760@gmail.com>
2024-05-03 15:53:03 +0300

    modal..

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-const Modal = ({ closeModal, children, imageUrl }) => {
+const Modal = ({ closeModal, children, imageUrl, width, height }) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center  ">
       <div className="absolute inset-0 bg-gray-900 opacity-75"></div>
-      <div className="relative bg-white border w-96 p-6 rounded-lg shadow-xl">
+      <div
+        className="relative bg-white border  p-5 rounded-lg shadow-xl"
+        style={{ maxWidth: width, maxHeight: height }}>
+        {" "}
         <button
-          className="absolute top-0 right-0 m-4 text-gray-500 hover:text-gray-800"
+          className="absolute top-0 right-0 m-7 p-2 text-[30px] text-gray-500 hover:text-gray-800"
           onClick={closeModal}>
           &times;
         </button>
